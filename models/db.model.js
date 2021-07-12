c48db9ffc98caf8daf77bc436684b075fd34d2e0
Osamamomani1 <momaniosama97@gmail.com>
2021-07-12 17:38:50 +0300

    almost dont

---
+++ models/db.model.js
@@ -0,0 +1,34 @@
+'use strict'
+const mongoose=require('mongoose');
+
+const DatalSchema = new mongoose.Schema({
+    nameartical:{type:String},
+    description:{type:String},
+    author:{type:String},
+    title:{type:String}
+})
+
+const artical= new mongoose.Schema({
+    email:{type:String},
+    blogs:[DatalSchema]
+})
+
+
+const articalModel=mongoose.model('community',artical  )
+
+//seeding
+const seedUser=()=>{
+    let osama= new articalModel ({
+        email:'momaniosama97@gmail.com',
+        blogs:[{nameartical :'sport',description:'book told you to climb to heigh elo in leage of legends as an adc roe',author:'gaming book',title:'copa amireca' },
+        {nameartical :'War and Peace',description:'A legendary masterpiece, this book is synonymous with difficult reading, so why not challenge yourshelf',author:'philosophical book',title:'erou'}  ]
+  
+    })
+
+  osama.save();
+  console.log(osama);
+  }
+
+  module.exports=articalModel
+                    
+//   seedUser();
--- server.js
+++ server.js
@@ -2,14 +2,60 @@ const express = require('express')
 const app = express() 
 const cors = require('cors');
 const axios = require('axios'); 
+const mongoose=require('mongoose');
+// const jwt=require('jsonwebtoken');
+// const jwksClient=require('jwks-rsa');
 app.use(cors()) 
 const handlerNews = require('./controller/user.controller')
 require('dotenv').config();
+const {articaltrollerfunc,
+    createArticalfunc,
+    updateBlog,
+    deleteBlog}= require('./controller/db.controller');
 
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect('mongodb://localhost:27017/community',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 // a server endpoint 
 app.get('/',function (req, res) {res.send('Hello World')})
 
  app.get('/news', handlerNews )
- 
+
+ app.get('/blogs',articaltrollerfunc);
+ app.post('/blogs',createArticalfunc);
+ app.put('/blogs/:blog_id',updateBlog);
+ app.delete('/blogs/:blog_id',deleteBlog);
+
+// ============================================Varfication Autho==============================================//
+
+//  const client = jwksClient({
+   
+//     jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+//   });
+
+
+// const getKey=(header, callback)=>{
+//     client.getSigningKey(header.kid, function(err, key) {
+//         const signingKey = key.publicKey || key.rsaPublicKey;
+//         callback(null, signingKey);
+//       });
+// }
+
+// app.get('/authorize',(req,res)=>{
+//     const token=req.headers.authorization.split(' ')[1];
+//     jwt.verify(token,getKey,{},(err,user)=>{
+//         if(err){
+//             res.send('invalid token');
+//         }
+//         res.send(user)
+//     })
+//     res.send(token);
+// });
+
+ // =====================================Varfication Autho===================================//
 app.listen(process.env.PORT,()=>{console.log(`listening to port: ${process.env.PORT}`);})
