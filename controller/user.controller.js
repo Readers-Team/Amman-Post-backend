2193d31c01d72db6b7e12b2fd857eb6fb1a43303
‘samahAbujwaied’ <samah.abujweed@yahoo.com>
2021-07-14 01:42:14 +0300

    final touch backend

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,7 +4,7 @@ const handlerNews =  (req,res) =>{
 const  Newsclass =require('../models/user.model')
 
 let searchQ = req.query.q;
-let newsURL = `https://newsapi.org/v2/everything?q=${searchQ}&apiKey=d34bef2baed3430aafd7b8dd320c3ac8`
+let newsURL = `https://newsapi.org/v2/everything?q=${searchQ}&apiKey=2c9673f9551b4c5687cf9d3ed5663b8b`
     
 axios
    .get(newsURL)
