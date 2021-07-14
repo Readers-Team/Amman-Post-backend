044349f2b26a44144b68c390feaa7f6777071545
qusaiqeisi <qusaiqeisi6@gmail.com>
2021-07-14 12:20:43 +0300

    lastupdate

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,7 +4,8 @@ const handlerNews =  (req,res) =>{
 const  Newsclass =require('../models/user.model')
 
 let searchQ = req.query.q;
-let newsURL = `https://newsapi.org/v2/everything?q=${searchQ}&apiKey=2c9673f9551b4c5687cf9d3ed5663b8b`
+let apiKey=process.env.REACT_APP_URL_API
+let newsURL = `https://newsapi.org/v2/everything?q=${searchQ}&apiKey=${apiKey}`
     
 axios
    .get(newsURL)
