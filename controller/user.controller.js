8e0977dea48a61860472142afc082224fe49559a
Osamamomani1 <momaniosama97@gmail.com>
2021-07-14 15:07:41 +0300

    bug

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,7 +4,7 @@ const handlerNews =  (req,res) =>{
 const  Newsclass =require('../models/user.model')
 
 let searchQ = req.query.q;
-let newsURL = `https://newsapi.org/v2/everything?q=${searchQ}&apiKey=2c9673f9551b4c5687cf9d3ed5663b8b`
+let newsURL = `https://newsapi.org/v2/everything?q=${searchQ}&apiKey=eab1af3cc80d46cdb4c839ce83f966cb`
     
 axios
    .get(newsURL)
